dc550d44c14c3cb916c7b59861a34af2baa0e2b6
Inobinky <bartblaak@chello.nl>
2020-06-21 15:41:54 +0200

    Added Full Integration Test

--- CircusTrain.Tests/IntegrationTests.cs
+++ CircusTrain.Tests/IntegrationTests.cs
@@ -52,5 +52,37 @@ namespace CircusTrain.Tests
             Assert.AreEqual(18, _animals.Count);
             Assert.AreEqual(10, _train.WagonCount);
         }
+
+        [TestMethod]
+        public void Full_Integration_Test()
+        {
+            // Arrange
+
+            // Act
+            for (int i = 0; i < 5; i++) { _animals.Add(AnimalFactory.SmallHerbivore); }
+            for (int i = 0; i < 5; i++) { _animals.Add(AnimalFactory.LargeHerbivore); }
+            for (int i = 0; i < 5; i++) { _animals.Add(AnimalFactory.MediumHerbivore); }
+            for (int i = 0; i < 2; i++) { _animals.Add(AnimalFactory.SmallCarnivore); }
+            for (int i = 0; i < 2; i++) { _animals.Add(AnimalFactory.MediumCarnivore); }
+            for (int i = 0; i < 2; i++) { _animals.Add(AnimalFactory.LargeCarnivore); }
+
+            _train.AddAnimals(_animals);
+            _train.DistributeAnimals();
+
+            // Assert
+            Assert.IsNotNull(_train);
+            Assert.IsNotNull(_animals);
+
+            foreach (Wagon wagon in _train.Wagons)
+            {
+                Assert.IsNotNull(wagon);
+                Assert.IsTrue(wagon.Animals.Any());
+            }
+
+            Assert.AreEqual(8, _train.WagonCount);
+            Assert.AreEqual(21, _animals.Count);
+            Assert.AreEqual(6, _animals.OfType<Carnivore>().Count());
+            Assert.AreEqual(15, _animals.OfType<Herbivore>().Count());
+        }
     }
 }
